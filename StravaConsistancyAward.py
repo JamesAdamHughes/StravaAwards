c41fe16d1fc876c6d11a50e402d6ca6e4c8d1e47
James Hughes <jh13293@my.bristol.ac.uk>
2017-06-24 11:54:42 +0100

    inital commit

---
+++ StravaConsistancyAward.py
@@ -0,0 +1,67 @@
+from datetime import datetime, timedelta, tzinfo
+import arrow
+
+class StravaConsistancyAward:   
+
+    def __init__ (self, name, eD, eW, eM, message, requiredActivites, eT):
+        self.epochDays          = eD
+        self.epochWeeks         = eW
+        self.epochMonths        = eM
+        self.name               = name
+        self.message            = message
+        self.requiredActivites  = requiredActivites
+        self.exerciseType = eT
+        
+        # Setup datetime used as "now", using timezone aware date
+        utc = UTC()
+        self.now = datetime.now(utc)
+
+    # Return the starting range of the award
+    # This should be the date of the start of the previous week or the previous month, 
+    # The start of the week is monday
+    def getStartDate(self):
+        if (self.epochDays != 0):
+            return self.now - timedelta(days=self.epochDays)
+        elif (self.epochWeeks != 0):
+            return self.now - timedelta(days=self.now.weekday(), weeks=(1 * (self.epochWeeks-1)))
+        else:
+            return self.now - timedelta(days=self.now.weekday(), weeks=self.epochMonths*4)
+
+    # Can't win awards for exercises done after "now"
+    def getEndDate(self):
+        return self.now
+
+    def getAwardType(self):
+        exerciseTypes = {
+            0: 'Run',
+            1: 'Ride'
+        }
+        return exerciseTypes[self.exerciseType]
+
+    def getAwardText(self):
+        return 'You won a {0} Award! {1}'.format(self.name, self.message)
+
+    # Manually set the time of "now"
+    # Used for testing different dates. Default is UTC
+    def setNow(self, year, month, day):        
+        string = datetime(year, month, day).isoformat(' ')
+        self.now = arrow.get(string, 'YYYY-M-D HH:mm:ss').replace(tzinfo='local')     
+        return   
+
+# A UTC class.
+class UTC(tzinfo):
+    """UTC"""
+
+    def utcoffset(self, dt):
+        ZERO = timedelta(0)
+        return ZERO
+
+    def tzname(self, dt):
+        return "UTC"
+
+    def dst(self, dt):
+        ZERO = timedelta(0)
+        return ZERO
+
+
+
---
+++ stravaAwards.py
@@ -0,0 +1,78 @@
+from emailUtilities import sendEmail, getEmailServer
+from stravalib.client import Client
+from StravaConsistancyAward import StravaConsistancyAward, UTC
+from pprint import pprint
+import arrow
+
+def getStravaClient():
+    client = Client()
+    client.access_token = 'eb7344dda517a55ca287f41e498005e13159cdc0'
+    return client
+
+
+def createAwards():
+    awards = []
+    # todo write this up in json and read from file
+    awards.append(StravaConsistancyAward('TwiceThisWeekAward', 0, 1, 0, 'You ran twice this week!', 2, 0))
+    awards.append(StravaConsistancyAward('Once This Week', 0, 1, 0, 'You ran once this week!', 1, 0))
+    awards.append(StravaConsistancyAward('TwoWeeksInARow', 0, 2, 0, 'You ran two weeks in a row!', 2, 0))
+    awards.append(StravaConsistancyAward('Four times a Month', 0, 0, 1, 'You ran four times this month!', 4, 0))
+
+    return awards
+
+
+def getAllActivites(stravaClient):
+    a = []
+    for activity in stravaClient.get_activities(after = '2017-05-1',  limit=200):
+        a.append(activity)
+    return a
+
+# Function takes an award type, applies it to the activites data
+# returns true if the award occured
+def testAwardOccured(award, activites):
+    print "Checking " + award.name
+
+    # Force set date for testing
+    award.setNow(2017, 05, 30)
+
+    print "Award time range: " + str(award.getStartDate()) + " to " + str(award.getEndDate());
+    print arrow.get(award.getStartDate()).humanize()
+
+    print "Valid Activites are..."
+    validActivities = []
+    for activity in activites:
+
+        activityStartDate = arrow.get(activity.start_date)
+
+        # Activity happened between now and now-eligiabletime
+        if(activityStartDate > award.getStartDate() and activityStartDate < award.getEndDate()):
+            if activity.type == award.getAwardType():
+                print '\t' + activity.name
+                validActivities.append(activity)
+
+    print "valid activites : " + str(len(validActivities))
+
+    if len(validActivities) == award.requiredActivites:
+        print award.getAwardText()        
+        return True
+    else:
+        return False
+
+
+print 'Starting Strava Awards'
+
+# create clients
+smtpserver = getEmailServer()
+stravaAwards = createAwards()
+stravaActivites = getAllActivites(getStravaClient())
+
+# Go through awards and award them
+for award in stravaAwards:
+    occured = testAwardOccured(award, stravaActivites)
+    if occured:
+        print 'Sending Email...'
+        sendEmail(smtpserver, award.name, award.getAwardText())
+    print '\n'
+
+
+
